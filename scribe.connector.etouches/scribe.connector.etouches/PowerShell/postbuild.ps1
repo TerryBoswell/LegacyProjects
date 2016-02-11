8c58abd6fed3b4f97ec2c81426ca3bd13961300c
TerryBoswell <trboswell74@comcast.net>
2016-02-11 11:39:34 -0500

    Fix for powershell script

--- scribe.connector.etouches/scribe.connector.etouches/PowerShell/postbuild.ps1
+++ scribe.connector.etouches/scribe.connector.etouches/PowerShell/postbuild.ps1
@@ -1,4 +1,4 @@
 stop-service -displayname "Scribe Online Agent"
 Remove-Item 'C:\Program Files (x86)\Scribe Software\Scribe Online Agent\Connectors\Scribe.Connector.eTouches\*' -Recurse
 Copy-Item 'E:\_git\etouches\scribe.connector.etouches\scribe.connector.etouches\bin\Debug\*' -Destination 'C:\Program Files (x86)\Scribe Software\Scribe Online Agent\Connectors\Scribe.Connector.eTouches' -Recurse
-stop-service -displayname "Scribe Online Agent"
+start-service -displayname "Scribe Online Agent"
